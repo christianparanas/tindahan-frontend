0c13199aa1fa24e99713888139f34b6df0348c81
Christian Paranas <christiannparanas@gmail.com>
2021-03-19 23:39:44 +0800

    added reg function with node, and have a toast if user successfully registered

--- pages/account/register.jsx
+++ pages/account/register.jsx
@@ -2,36 +2,63 @@ import Footer from '../../components/Footer'
 import Nav from '../../components/Nav'
 import Link from 'next/link'
 
+import axios from 'axios' 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { useForm } from "react-hook-form";
+
+
 function register() {
+		// react hool form
+		const { register, handleSubmit, watch, errors } = useForm();
+
+		//  toast config
+		const notify = () => toast.success("Registered Successfully!", {
+			autoClose: 3000,
+		});
+
+  	const onSubmit = (data, e) => {
+  		axios.post("http://localhost:3001/api/register", {
+				name: data.name,
+				email: data.email,
+				password: data.password,
+			})
+
+  		// show toast
+  		notify()
+  		// clear inputs after submit
+			e.target.reset();
+  	}
+
+
+
 		return (
 		<div className="loginFormWrapper">
+			 <ToastContainer />
 			<Nav />
 			<div className="loginContentWrapper">
 				<h1 className="header">CREATE ACCOUNT</h1>
-				<form action="">
+				<form onSubmit={handleSubmit(onSubmit)}>
 					<div className="loginEmailWrapper">
-						<label htmlFor="regfirstname">FIRST NAME</label>
-						<input type="text" id="regfirstname" name="regfirstname" />
-					</div>
-					<div className="loginEmailWrapper">
-						<label htmlFor="reglastname">LAST NAME</label>
-						<input type="text" id="reglastname" name="reglastname" />
+						<label htmlFor="regfirstname">FULL NAME</label>
+						<input name="name" ref={register({ required: true })} type="text" id="regname" />
 					</div>
 					<div className="loginEmailWrapper">
 						<label htmlFor="regemail">EMAIL</label>
-						<input type="email" id="regemail" name="regemail" />
+						<input name="email" ref={register({ required: true })} type="email" id="regemail" />
 					</div>
 					<div className="loginEmailWrapper">
 						<label htmlFor="regpass">PASSWORD</label>
-						<input type="email" id="regpass" name="regpass" />
+						<input name="password" ref={register({ required: true })} type="password" id="regpass" />
 					</div>
 					<div className="terms">
 						<label htmlFor="chkagree" className="terms">
-	             <input id="chkagree" type="checkbox" />
+	            <input id="chkagree" type="checkbox" />
 	               By ticking this box, you confirm that you have read, understood and agreed to our Terms and Conditions and the collection, use, storage of your data in accordance with our Privacy Policy
 	          </label>
           </div>
-					<input type="submit" value="CREATE" className="loginSignIpBtn" />
+          <input type="submit" value="Create" className="loginSignIpBtn" />
 				</form>
 			</div>
 			<Footer />
