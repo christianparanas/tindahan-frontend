29072e2d1188228f541cfeefb5c44d2d522124f7
Christian Paranas <christiannparanas@gmail.com>
2021-06-16 00:13:44 +0800

    spe customer

--- pages/admin/customers/[customerId].jsx
+++ pages/admin/customers/[customerId].jsx
@@ -72,6 +72,9 @@ export default function Customer() {
 				<div className="admin_customer">
 					<ToastContainer />
 					<Adminnav />
+					<Link href="/admin/customers">
+						<svg className="back_btn" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path></svg>
+					</Link>
 
 					{hasCustomerInDB ? 
 						(
